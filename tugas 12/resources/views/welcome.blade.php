a5246c865609fbfcfdad32e313b813f9bb8a75ab
Ar-Razy Muhammad <arrazymuhammad@live.com>
2020-07-01 17:40:33 +0700

    mengerjakan tugas 12

---
+++ tugas 12/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SanberBook</title>
+</head>
+<body style="text-align : center">
+
+    <h1>SELAMAT DATANG!</h1>
+    <h2>{{$f_name}} {{$l_name}}</h2>
+    <h3>Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</h3>
+</body>
+</html>
